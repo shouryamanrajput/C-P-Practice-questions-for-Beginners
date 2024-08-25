08dd5a893bc61e30d0ba8c605621b4ddd5967a54
Shouryaman Singh Langeh <122371427+shouryamanrajput@users.noreply.github.com>
2024-08-25 14:14:59 +0530

    Delete C++ Basic Progs directory

--- C++ Basic Progs/2. Use of endl in a program.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main() {
-	cout << "Name: Shouryaman";
-	cout << endl;                       // endl stands for endline ...(ENTER)
-	cout << "RollNumber: 151";
-	cout << endl;
-	cout << "Section : A4";
-}
--- C++ Basic Progs/3. Displaying the Number.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-int main() {
-	cout << "Number : 4+2";
-	cout << endl;
-	cout << "Answer : " << 4+2;
-}
